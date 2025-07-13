3441abe3cebf3ae5cb3df152e1a110772795d34b
DevLucia-21 <21_pg@naver.com>
2025-07-13 19:13:44 +0900

    chore: 프로젝트 기본 설정 완료

---
+++ src/main/java/com/fluxion/sote/SoteApplication.java
@@ -0,0 +1,13 @@
+package com.fluxion.sote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluxion/sote/SoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxion.sote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
