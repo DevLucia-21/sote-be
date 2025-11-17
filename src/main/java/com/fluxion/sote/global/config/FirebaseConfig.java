c225d006c2befe1e131bb6e0bad0e1506016adea
DevLucia-21 <21_pg@naver.com>
2025-11-17 15:47:16 +0900

    fix(firebase): load service account from external file instead of classpath

--- src/main/java/com/fluxion/sote/global/config/FirebaseConfig.java
+++ src/main/java/com/fluxion/sote/global/config/FirebaseConfig.java
@@ -4,19 +4,25 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${gcp.firebase.credentials.location}")
+    private String firebaseCredentialsPath;
+
     @PostConstruct
     public void initialize() {
         try {
-            InputStream serviceAccount = getClass().getClassLoader()
-                    .getResourceAsStream("firebase/firebase-service-account.json");
+
+            System.out.println("Firebase credentials path = " + firebaseCredentialsPath);
+
+            FileInputStream serviceAccount = new FileInputStream(firebaseCredentialsPath.replace("file:", ""));
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -27,7 +33,7 @@ public class FirebaseConfig {
                 System.out.println("Firebase initialized successfully.");
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.err.println("Firebase initialization failed: " + e.getMessage());
         }
     }
