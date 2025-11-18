9b8a4b2c381301a8a37dcd31d624cc6710ab35a3
DevLucia-21 <21_pg@naver.com>
2025-11-18 20:45:01 +0900

    fix(badge): update badge descriptions from '화남' to '분노'

---
+++ src/main/resources/db/migration/V41__update_badge_description_anger_text.sql
@@ -0,0 +1,15 @@
+UPDATE badge_definition
+SET description = '분노 챌린지를 10회 완료'
+WHERE name = '분노 조절의 달인 I';
+
+UPDATE badge_definition
+SET description = '분노 챌린지를 20회 완료'
+WHERE name = '분노 조절의 달인 II';
+
+UPDATE badge_definition
+SET description = '분노 챌린지를 30회 완료'
+WHERE name = '분노 조절의 달인 III';
+
+UPDATE badge_definition
+SET description = '분노 챌린지를 50회 이상 완료'
+WHERE name = '분노 조절의 마스터';
